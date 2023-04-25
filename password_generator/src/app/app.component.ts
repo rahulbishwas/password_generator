a49a767587de5649b5ebe86b8b01ce9fb3f55a11
rahulbishwas <bishwascvrc1216@gmail.com>
2023-04-25 19:25:05 +0530

    third commit

--- password_generator/src/app/app.component.ts
+++ password_generator/src/app/app.component.ts
@@ -9,9 +9,38 @@ export class AppComponent {
   title = 'password_generator';
 
   password='';
+  includeLetters=false;
+  includeNumber=false;
+  includeSymbols=false;
+  length=0;
 
   onButtonClick(){
+    console.log(this.includeSymbols);
+    console.log(this.length)
 this.password='MY PASSWORD'
+
+  }
+
+  onChangeUseLetters(){
+
+this.includeLetters=!this.includeLetters;
+
+  }
+  onChangeUseNumber(){
+    this.includeNumber=!this.includeNumber;
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+
+  onChangeLength(value:string){
+    const parseValue=parseInt(value)
+// this.length=value;
+
+if(!isNaN(parseValue)){
+this.length=parseValue;
+
+}
 
   }
 }
